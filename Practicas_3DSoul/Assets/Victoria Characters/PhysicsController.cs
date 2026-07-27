f708896daf8e6edf44e9ed2f542bc8babd96aea6
victorialpze <victorialopezespla@gmail.com>
2026-07-27 16:11:43 +0200

    prueba

---
+++ Practicas_3DSoul/Assets/Victoria Characters/PhysicsController.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+
+public class PhysicsController : MonoBehaviour
+{
+    public float speed = 5f;
+    public Transform model;
+    public float rotationSpeed = 8f;
+
+    private Vector3 moveInput;
+    private bool isWalking;
+
+    void Update()
+    {
+        Debug.Log("Update Physics");
+        float moveX = 0f;
+        float moveZ = 0f;
+
+        if (Input.GetKey(KeyCode.W)) moveZ = 1f;
+        if (Input.GetKey(KeyCode.A)) moveX = -1f;
+        if (Input.GetKey(KeyCode.D)) moveX = 1f;
+        if (Input.GetKey(KeyCode.S)) moveZ = -1f;
+
+        moveInput = new Vector3(moveX, 0, moveZ).normalized;
+        isWalking = moveInput != Vector3.zero;
+        
+        if (isWalking && model != null)
+        {
+            Quaternion look = Quaternion.LookRotation(moveInput);
+            Quaternion offset = Quaternion.Euler(-90f, 0f, 0f);
+
+            model.rotation = Quaternion.Slerp(
+                model.rotation,
+                look * offset,
+                rotationSpeed * Time.deltaTime
+            );
+        }
+
+        
+    }
+
+    void FixedUpdate()
+    {
+        Debug.Log(moveInput);
+        transform.Translate(moveInput * speed * Time.fixedDeltaTime, Space.World);
+    }
+
+    public bool IsWalking()
+    {
+        return isWalking;
+    }
+}
